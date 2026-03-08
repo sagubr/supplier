0654346fb525f5efbd5137c9ccd13d34cc8b7cf2
Gustavo Garcia <sousagustavogarcia@gmail.com>
2026-03-07 23:39:24 -0300

    chore: add try catch no provider sendrgrid

--- src/infra/email/providers/sendgrid.provider.ts
+++ src/infra/email/providers/sendgrid.provider.ts
@@ -1,6 +1,7 @@
-import sgMail from "@sendgrid/mail";
+import sgMail, { ResponseError } from "@sendgrid/mail";
 import { IEmailProvider, EmailPayload } from "../email.interface";
 import { env } from "@/config/env.config";
+import { logger } from "@/infra/observability/logger.config";
 
 export class SendgridProvider implements IEmailProvider {
 	constructor() {
@@ -12,17 +13,27 @@ export class SendgridProvider implements IEmailProvider {
 
 	async send(payload: EmailPayload): Promise<void> {
 		if (!payload.body || !payload.to || !payload.subject) {
-			throw new Error("Email payload incomplete: to, subject and html are required");
+			throw new Error(
+				"Email payload incomplete: to, subject and html are required",
+			);
 		}
 
-		await sgMail.send({
-			to: payload.to,
-			from: env.EMAIL_FROM_NAME,
-			subject: payload.subject,
-			html: payload.body,
-			cc: payload.cc,
-			bcc: payload.bcc,
-			replyTo: payload.replyTo,
-		});
+		try {
+			await sgMail.send({
+				to: payload.to,
+				from: env.EMAIL_FROM_NAME,
+				subject: payload.subject,
+				html: payload.body,
+				cc: payload.cc,
+				bcc: payload.bcc,
+				replyTo: payload.replyTo,
+			});
+		} catch (error: ResponseError | any) {
+			if (error.response && error.response.body) {
+				logger.error(JSON.stringify(error.response.body, null, 2));
+			} else {
+				logger.error("Erro desconhecido:", error);
+			}
+		}
 	}
 }
